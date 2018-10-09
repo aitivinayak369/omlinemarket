03d3505924f1c55b81cfa35da66975e4afea87e5
aitivinayak369 <aitivinayak369@gmail.com>
2018-10-09 22:47:19 +0530

    onlinemarket 2.0.1

---
+++ secrets/secretkeys.js
@@ -0,0 +1,16 @@
+
+
+var s={}
+
+
+s.secret="secret";
+
+
+//facebooks
+s.fbKey="252826862091631";
+s.fbId= "8a7421bb45bfbdca55f938923cc61eca";
+
+//google
+s.googleKey="1081682415438-io8jl6c8jn4t0308atj9sffko47hsq26.apps.googleusercontent.com";
+s.googleId="vLhibI1wFPGyUar3K1Q5E-Kv";
+module.exports=s;
