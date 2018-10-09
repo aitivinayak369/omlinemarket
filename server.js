4dcfde400202e3bdb19d3b578a997c6f7bddbc81
aitivinayak369 <aitivinayak369@gmail.com>
2018-10-10 00:15:55 +0530

    onlinemarket 2.0

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ var app = express();
 
 app.use(express.static(path.join(__dirname,'./public')))
 
-var options ={
-   key:fs.readFileSync('./server.key'),
-   cert:fs.readFileSync('./server.crt'),
-   passphrase:'vinayak',
-   requestcert:false,
-   rejectUnauthorized:false
+// var options ={
+//    key:fs.readFileSync('./server.key'),
+//    cert:fs.readFileSync('./server.crt'),
+//    passphrase:'vinayak',
+//    requestcert:false,
+//    rejectUnauthorized:false
 
-}
+// }
 
-var server = https.createServer(options,app).listen(port,"0.0.0.0",()=>{
-    console.log('listening at port:',port);
-})
+// var server = https.createServer(options,app).listen(port,"0.0.0.0",()=>{
+//     console.log('listening at port:',port);
+// })
 //session connect-mongo
 
 app.use(session({secret:secretFile.secret,saveUninitialized:true,resave:true,store:new MongoStore({url:'mongodb://vinayak:vinayak123@ds052978.mlab.com:52978/ecommerce'})}));
@@ -44,13 +44,13 @@ var mainRouter = require('./controller/main');
 app.use('/',mainRouter);
 
 
-// app.listen(3030,(err)=>{
-//     if(err)
-//     {
+ app.listen(port,(err)=>{
+     if(err)
+    {
       
-//         console.log("Error occured in binding port:"+err);
-//     }
-//     else{
-//         console.log('Listening on port: '+3030);
-//     }
-// })
+        console.log("Error occured in binding port:"+err);
+    }
+    else{
+        console.log('Listening on port: '+3030);
+    }
+})
